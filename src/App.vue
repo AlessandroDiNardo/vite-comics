5f5f1d89c54c60a7eb1dc0809f8a3b972dbb21a2
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-12 16:38:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style lang="scss">
+@use './styles/partials/_variables.scss' as *;
+@use './styles/general.scss' as *;
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "Header",
+
+}
+</script>
+
+<template>
+    <header>
+        <a href="#">
+            <img src="./assets/img/dc-logo.png" alt="">
+        </a>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    text-align: center;
+}
+</style>
