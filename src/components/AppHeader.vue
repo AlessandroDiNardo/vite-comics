57e4577fe1dacb0191e7a196f69de6c6c8339887
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-13 12:00:47 +0100

    apportate modifiche all'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,42 +7,52 @@ export default {
                 {
                     text: "CHARACTERS",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "COMICS",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "MOVIES",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "TV",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "GAMES",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "COLLECTIBLES",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "VIDEOS",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "FANS",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "NEWS",
                     url: "#",
+                    current: "false",
                 },
                 {
                     text: "SHOP",
                     url: "#",
+                    current: "false",
                 },
             ]
         }
@@ -51,52 +61,68 @@ export default {
 </script>
 
 <template>
-    <header>
+    <section>
         <div class="container">
             <div class="logo-side">
                 <a href="#">
-                    <img src="../assets/img/dc-logo.png" alt="">
+                    <img src="/img/dc-logo.png" alt="">
                 </a>
             </div>
-            <ul>
-                <li v-for="(link, index) in links" :key="index">
-                    <a href="#">
-                        {{ link.text }}
-                    </a>
-                </li>
-            </ul>
+            <div>
+                <ul>
+                    <li v-for="(link, index) in links" :key="index">
+                        <a href="#" :class="{ active: links.current }">
+                            {{ link.text }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </header>
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables.scss' as *;
 @use '../styles/general.scss' as *;
 
-header {
+section {
     background-color: white;
 
     .container {
         display: flex;
         align-items: center;
         justify-content: space-between;
-    }
+        padding-top: 20px;
 
-    ul {
-        list-style: none;
-        display: flex;
-        justify-content: space-around;
-        gap: 2em;
-        font-size: 13px;
+        .logo-side {
+            height: 100%;
 
-        li a {
-            color: rgb(0, 0, 0);
-            font-weight: 600;
-            text-decoration: none;
+            img {
+                width: 90%;
+            }
+        }
 
-            &.active,
-            &:hover {
-                color: $primary;
+        ul {
+            list-style: none;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            gap: 2em;
+            font-size: 13px;
+
+            li a {
+                color: rgb(0, 0, 0);
+                font-weight: 700;
+                text-decoration: none;
+                display: flex;
+                align-items: center;
+                height: 110px;
+
+                &.active,
+                &:hover {
+                    color: $primary;
+                    border-bottom: 4px solid $primary;
+                }
             }
         }
     }
